b90f2b1e5e888e63f9922ee89cc40ce4fe7c497a
Himanshu <himanshujain5690174@gmail.com>
2024-02-09 09:30:44 +0530

    model update

---
+++ server/models/post.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+    {
+
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -24,5 +24,53 @@ const userSchema = new mongoose.Schema(
             required: true,
             trim: true,
         },
+        profileImg: {
+            type: String,
+        },
+        additionalDetail: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "AdditionalDetails"
+        },
+        following: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ],
+        followers: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ],
+        bookmarked: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Post"
+            }
+        ],
+        posts: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Post"
+            }
+        ],
+        refreshToken: {
+            type: String
+        },
+        accessToken: {
+            type: String
+        },
+        isVerified: {
+            type: Boolean,
+            default: false
+        },
+    },
+    {
+        timestamps: true
     }
-)
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
