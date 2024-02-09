8d5c6794f3b11b91c8c86013ba2919f5b2b137ed
Himanshu <himanshujain5690174@gmail.com>
2024-02-09 19:34:58 +0530

    models update

---
+++ server/models/commeent.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        description: {
+            type: String,
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+export default Comment;
--- server/models/post.model.js
+++ server/models/post.model.js
@@ -2,7 +2,30 @@ import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema(
     {
-
+        postUrl: {
+            type: String
+        },
+        description: {
+            type: String,
+        },
+        likes: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ],
+        bookmarks: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ],
+        comments: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Comment"
+            }
+        ]
     },
     {
         timestamps: true
