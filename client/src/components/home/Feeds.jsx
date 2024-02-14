4834ee8b59ad94f36f905694774f3873f1664c74
Himanshu <himanshujain5690174@gmail.com>
2024-02-14 13:43:39 +0530

    navbar complete

--- client/src/components/home/Feeds.jsx
+++ client/src/components/home/Feeds.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { IoSettingsOutline } from "react-icons/io5";
 
 const Feeds = () => {
 
@@ -16,23 +17,28 @@ const Feeds = () => {
             <div className='flex flex-col  justify-start items-center bg-black'>
 
                 {/* navbar */}
-                <div className='flex flex-row justify-between w-full border-b-2 border-[white]/[0.19]'>
+                <div className='flex flex-row justify-between backdrop-blur-2xl w-full border-b-2 border-[white]/[0.19]'>
                     {
                         navLinks?.map((navlink, index) => (
                             <div
                                 key={index}
-                                className='flex w-full hover:bg-[white]/[0.1] cursor-pointer justify-center items-center flex-row gap-2'
+                                className='flex w-full hover:bg-[white]/[0.1] pt-3 transition-all duration-200 ease-in-out  cursor-pointer justify-center items-center flex-row gap-2'
                                 onClick={() => {
                                     setOption(navlink)
                                 }}
                             >
                                 <div className='flex flex-col gap-2 justify-center items-center'>
-                                    <p className={` ${option === navlink ? "text-white font-bold" : "text-[white]/[0.4] font-semibold"} `}>{navlink}</p>
-                                    <span className={`bg-blue-400 w-full h-1 rounded-full ${option === navlink ? "block" : "hidden"} `} />
+                                    <p className={` transition-all duration-300 ease-in-out ${option === navlink ? "text-white font-bold" : "text-[white]/[0.4] font-semibold"} `}>{navlink}</p>
+                                    <span className={`bg-blue-400 transition-all duration-200 ease-in-out w-full h-1 rounded-full ${option === navlink ? "block" : "hidden"} `} />
                                 </div>
                             </div>
                         ))
                     }
+
+
+                    <div className='hover:bg-[white]/[0.1] transition-all duration-300 ease-in-out text-xl mx-2 text-white px-3 rounded-full flex justify-center items-center cursor-pointer '>
+                        <IoSettingsOutline />
+                    </div>
 
                 </div>
 
