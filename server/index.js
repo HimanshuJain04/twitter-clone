a6ae2d39fbdce680a7755a4dd922cd67f8e4cc4f
Himanshu <himanshujain5690174@gmail.com>
2024-03-10 09:53:02 +0530

    sockeet-io setting up

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import { dbConnection } from "./config/dbConnection.js"
 import dotenv from "dotenv";
 import { app } from "./app.js";
 import { cloudinaryConnection } from "./config/cloudinary.config.js"
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 
 // import routes 
@@ -28,9 +30,36 @@ app.use("/api/v1/post", postRoute);
 app.use("/api/v1/user", userRoute);
 
 
-app.listen(PORT, () => {
+const server = createServer(app);
+const io = new Server(server);
+
+
+// Handle WebSocket connections
+io.on('connection', socket => {
+    console.log('New client connected:', socket);
+
+    // Handle signaling messages
+    socket.on('offer', (offer, to) => {
+        io.to(to).emit('offer', offer, socket.id);
+    });
+
+    socket.on('answer', (answer, to) => {
+        io.to(to).emit('answer', answer);
+    });
+
+    socket.on('icecandidate', (candidate, to) => {
+        io.to(to).emit('icecandidate', candidate);
+    });
+
+    // Handle disconnection
+    socket.on('disconnect', () => {
+        console.log('Client disconnected:', socket.id);
+    });
+});
+
+server.listen(PORT, () => {
     console.log("Server listening on port : ", PORT)
-})
+});
 
 
 app.get("/api/v1/", (req, res) => {
