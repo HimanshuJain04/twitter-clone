7a1e6f4536d2ac56725f43386cb95c66e9b3360d
Himanshu <himanshujain5690174@gmail.com>
2024-03-11 17:19:45 +0530

    bottom Navbar update

--- client/src/components/ProtectedRoutes.jsx
+++ client/src/components/ProtectedRoutes.jsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux"
 import Landing from '../pages/Landing';
 import FeatureSidebar from './common/FeaturSidebar';
 import TrendingSidebar from './common/TrendingSidebar';
+import MobileNavbar from "./MobileNavbar";
 
 const ProtectedRoutes = () => {
     const userAuth = useSelector(state => state.auth.user);
@@ -11,7 +12,7 @@ const ProtectedRoutes = () => {
 
     return (
         userAuth ?
-            <div className='justify-between h-screen overflow-hidden w-full items-start flex flex-row'>
+            <div className='justify-between relative h-screen overflow-hidden w-full items-start flex flex-row'>
                 <div className='lg:block hidden'>
                     <FeatureSidebar />
                 </div>
@@ -20,6 +21,9 @@ const ProtectedRoutes = () => {
                 </div>
                 <div className='xl:block hidden'>
                     <TrendingSidebar />
+                </div>
+                <div className='fixed lg:hidden block left-0 bottom-0 w-full'>
+                    <MobileNavbar />
                 </div>
             </div>
             : <Landing />
