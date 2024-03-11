bc8b881482960ed76c55eb6fdd59512e86828d62
Himanshu <himanshujain5690174@gmail.com>
2024-03-11 13:25:43 +0530

    update cors

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(cors(
     {
-        origin: "http://localhost:3000",
+        origin: "https://twitter-himanshu.vercel.app",
         credentials: true,
     }
 ));
