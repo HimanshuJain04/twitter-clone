fb77a6f19932a6885290f33fa0edbb5e531b77c0
Himanshu <himanshujain5690174@gmail.com>
2024-03-12 12:16:55 +0530

    updated Model

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -19,8 +19,16 @@ router.route("/updateUserDetails").patch(verifyJwtToken, updateUserDetails);
 router.route("/fetch-users").get(fetchSearches);
 router.route("/userFollowHandler/:anotherUserId").patch(verifyJwtToken, userFollow);
 router.route("/updateUserCoverImage").post(verifyJwtToken, updateUserCoverImage);
-
 router.route("/get-all-connected-user").get(verifyJwtToken, getAllConnectedUsers);
+
+// ****************** CHAT *****************
+// router.route("/chat").post(accessChats);
+// router.route("/chat").get(fetchChats);
+// router.route("/chat/create-group").post(createGroupChats);
+// router.route("/chat/rename-group").post(createGroupChats);
+// router.route("/chat/remove-group").post(createGroupChats);
+// router.route("/chat/add-group").post(createGroupChats);
+
 
 
 export default router;
