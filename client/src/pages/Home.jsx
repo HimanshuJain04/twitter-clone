3a772e900704ab9e3269adeff6714438bf3af925
Himanshu <himanshujain5690174@gmail.com>
2024-03-13 21:04:42 +0530

    update sidebar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { IoSettingsOutline } from "react-icons/io5";
 import Post from '../components/Post.jsx';
 import CreatePost from '../components/CreatePost.jsx';
@@ -7,8 +7,10 @@ import TransparencySpinner from '../components/common/TransparencySpinner.jsx';
 import PostSkeleton from '../components/common/PostSkeleton.jsx';
 import { MdOutlineAdd } from "react-icons/md";
 import { MdOutlineClose } from "react-icons/md";
-
-
+import logo from "/logo.png"
+import { useSelector } from "react-redux"
+import Sidebar from "../components/common/FeaturSidebar.jsx"
+import { RxCross1 } from "react-icons/rx";
 
 const navLinks = [
     "For you",
@@ -21,9 +23,12 @@ const Home = () => {
 
     const [allPosts, setAllPosts] = useState([]);
     const [isSkeleton, setIsSkeleton] = useState(false);
+    const scrollDiv = useRef(null);
     const [hasMore, setHasMore] = useState(true);
     const [index, setIndex] = useState(0);
     const [openCreatePost, setOpenCreatePost] = useState(false);
+    const [showSidebar, setShowSidebar] = useState(false);
+    const userState = useSelector(state => state.auth.user);
 
     const [option, setOption] = useState(navLinks[0]);
 
@@ -90,37 +95,85 @@ const Home = () => {
                     )
                 }
 
-                {/* navbar */}
-                <div className='flex sticky  top-0 flex-row justify-between backdrop-blur-2xl w-full border-b-2 border-[white]/[0.19]'>
-                    {
-                        navLinks?.map((navlink, index) => (
-                            <div
-                                key={index}
-                                className='flex w-full hover:bg-[white]/[0.1] pt-3 transition-all duration-200 ease-in-out  cursor-pointer justify-center items-center flex-row gap-2'
-                                onClick={() => {
-                                    setOption(navlink)
-                                }}
-                            >
-                                <div className='flex flex-col gap-2 justify-center items-center'>
-                                    <p className={` transition-all duration-300 ease-in-out ${option === navlink ? "text-white font-bold" : "text-[white]/[0.4] font-semibold"} `}>{navlink}</p>
-                                    <span className={`bg-blue-400 transition-all duration-200 ease-in-out w-full h-1 rounded-full ${option === navlink ? "block" : "hidden"} `} />
-                                </div>
+                {
+                    showSidebar && (
+                        <div className='w-full z-[100] bg-black/[0.3] absolute justify-start items-start flex'>
+                            <div className='w-[300px] relative pl-5 bg-black transition-transform duration-1000 transform translate-x-0'>
+                                <Sidebar />
+                                <span
+                                    onClick={() => setShowSidebar(false)}
+                                    className='text-white text-2xl right-5 absolute top-5'
+                                >
+                                    <RxCross1 />
+                                </span>
                             </div>
-                        ))
-                    }
+                        </div>
+                    )
+                }
 
+                {/* navbar */}
+                <div className={`sticky top-0 w-full backdrop-blur-2xl border-b-2 border-[white]/[0.19]  `}>
 
-                    <div className='hover:bg-[white]/[0.1] transition-all duration-300 ease-in-out text-xl mx-2 text-white px-3 rounded-full flex justify-center items-center cursor-pointer '>
-                        <IoSettingsOutline />
+                    {/* mobile navbar */}
+                    <div className='w-full px-2 py-1 flex flex-row justify-between items-center'>
+
+                        <div onClick={() => setShowSidebar(true)} className='w-10 h-10 overflow-hidden'>
+                            <img
+                                src={userState.profileImg}
+                                alt="profile"
+                                className='w-full h-full object-contain rounded-full'
+                            />
+                        </div>
+
+                        <div className='h-6 overflow-hidden'>
+                            <img
+                                src={logo}
+                                alt="logo"
+                                className='object-contain h-full w-auto'
+                            />
+                        </div>
+
+                        <div>
+                            <span className='text-white text-2xl'>
+                                <IoSettingsOutline />
+                            </span>
+                        </div>
                     </div>
 
-                </div>
+                    {/* follow and following */}
+                    <div className='flex flex-row justify-between w-full '>
+                        {
+                            navLinks?.map((navlink, index) => (
+                                <div
+                                    key={index}
+                                    className='flex w-full hover:bg-[white]/[0.1] pt-3 transition-all duration-200 ease-in-out  cursor-pointer justify-center items-center flex-row gap-2'
+                                    onClick={() => {
+                                        setOption(navlink)
+                                    }}
+                                >
+                                    <div className='flex flex-col gap-2 justify-center items-center'>
+                                        <p className={` transition-all duration-300 ease-in-out ${option === navlink ? "text-white font-bold" : "text-[white]/[0.4] font-semibold"} `}>{navlink}</p>
+                                        <span className={`bg-blue-400 transition-all duration-200 ease-in-out w-full h-1 rounded-full ${option === navlink ? "block" : "hidden"} `} />
+                                    </div>
+                                </div >
+                            ))
+                        }
+
+
+                        <div className='hover:bg-[white]/[0.1] lg:flex hidden  transition-all duration-300 ease-in-out text-xl mx-2 text-white px-3 rounded-full  justify-center items-center cursor-pointer '>
+                            <IoSettingsOutline />
+                        </div>
+
+                    </div >
+                </div >
+
+
 
                 {/* Feeds */}
-                <div className='h-[calc(100vh-100px)] w-full bg- overflow-y-auto'>
+                <div ref={scrollDiv} className='h-[calc(100vh-100px)] w-full bg- overflow-y-auto' >
                     {
                         isSkeleton ? (
-                            <div className='flex flex-col justify-start gap-5 items-center'>
+                            <div className='flex flex-col justify-start gap-5 items-center' >
                                 <PostSkeleton />
                                 <PostSkeleton />
                                 <PostSkeleton />
@@ -157,16 +210,16 @@ const Home = () => {
                     }
 
 
-                </div>
+                </div >
 
                 {/* Create post button */}
-                <button onClick={() => { setOpenCreatePost(true) }} className='fixed lg:hidden right-5 bottom-16 rounded-full bg-blue-500 p-2 sm:p-4 shadow-md shadow-black '>
+                <button onClick={() => { setOpenCreatePost(true) }} className='fixed lg:hidden right-5 bottom-16 rounded-full bg-blue-500 p-2 sm:p-4 shadow-md shadow-black ' >
                     <span className=' text-white text-4xl '>
                         <MdOutlineAdd />
                     </span>
-                </button>
+                </button >
 
-            </div>
+            </div >
         </div >
     );
 }
